dc07fc049e16d9f36bc9f60fe02fcc03a0177b33
s-kalaus <sergey.kalaus@gmail.com>
2024-01-21 18:24:43 +0100

    pm2 logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'weather-backend',
-      script: 'server',
+      script: 'server.js',
       instances: 1,
       autorestart: true,
       watch: false,
